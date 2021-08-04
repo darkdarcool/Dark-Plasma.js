d3b41c92d3daae7cb88639eaa8dabce3eeb7ef73
darkdarcool <darrendreddy@gmail.com>
2021-08-04 15:37:13 -0400

    Added err paths like 404

--- packages/dark-plasma-router/src/index.js
+++ packages/dark-plasma-router/src/index.js
@@ -5,5 +5,17 @@ export function getRoutes() {
 }
 
 export function createPath(path, html) {
+  if (isNaN(path)) {
+    console.log("Cannot name path after HTML 404 codes. Use createErrPath()")
+    process.exit(0)
+  }
   routes[`${path}`] = html;
+}
+
+export function createErrPath(path, html) {
+  if (typeof path !== number) {
+    console.log("Must be HTML err code")
+    process.exit(0)
+  }
+  routes[`${path}`] = html; // Must be string for path in routes because that is how plasma start calls/finds it!
 }
